4b5d77b5c14ffc674fdd1a5b4291947db4c110ab
AleEnjandro <alejandromartinez.alu@iespacomolla.es>
2021-09-14 17:41:06 +0200

    ejB.js comentado

--- Practica2.01/ejB.js
+++ Practica2.01/ejB.js
@@ -1,7 +1,7 @@
 "use strict";
 //Declaración de variables en forma de array
 var juan=[116,97,123];
-var miguel=[116,97,122];
+var miguel=[116,97,123];
 var maria=[97,134,105];
 //Cálculo de media de todos
 var mediaJuan=(juan[0]+juan[1]+juan[2])/3;
@@ -13,13 +13,14 @@ if (mediaJuan>mediaMiguel && mediaJuan>mediaMaria){
     console.log("Gana Juan "+mediaJuan);
 } else {
     if (mediaJuan<mediaMiguel && mediaMiguel>mediaMaria){
-        console.log("Gana Miguel "+mediaMiguel);
+        console.log("Gana Miguel "+mediaMiguel); //Comprobación de victoria
     } else {
         if (mediaMaria>mediaMiguel && mediaJuan<mediaMaria){
-            console.log("Gana Maria "+mediaMaria);
+            console.log("Gana Maria "+mediaMaria); //Comprobación de victoria
         } else{
+            //Comprobaciones de empate
             if (mediaMaria==mediaMiguel && mediaJuan==mediaMiguel){
-                console.log("Empate "+mediaMaria);
+                console.log("Empate entre todos "+mediaMaria);
             }
             if (mediaJuan<mediaMiguel){
                 console.log("Empate entre Miguel y María "+mediaMiguel);
