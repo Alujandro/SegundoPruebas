5d5cddb4e95c011c2380fcac74e3c9f50a1b8fa2
AleEnjandro <alejandromartinez.alu@iespacomolla.es>
2021-09-16 19:43:59 +0200

    Practica 2 funciona y comentadas ambas prácticas

--- Practica2.02/Practica1.js
+++ Practica2.02/Practica1.js
@@ -1,19 +1,18 @@
 "use strict";
-function sumamiento(){
+function sumamiento(){  //Declaración de la función suma
     var total=0;
-    for (var i=0; i<arguments.length; i++){
-        
-        if (arguments.length<2){
-            return "ERROR GARRAFAL: Parámetros insuficientes";
-        } else {
-            if (typeof arguments[i] != typeof 1){
+    if (arguments.length<2){    //Comparación de cantidad de parámetros introducidos
+        return "ERROR GARRAFAL: Parámetros insuficientes";
+    } else {
+        for (var i=0; i<arguments.length; i++){     //Bucle que recorre los parámetros introducidos
+            if (typeof arguments[i] != typeof 1){   //Comprobación de tipo de parámetro
                 return `ERROR GARRAFAL: El parámetro ${i} no es un número`;
             } else {
-                total=arguments[i]+total;
+                total=arguments[i]+total;   //Suma de parámetros
             }
         }
+        return total;
     }
-    return total;
 }
 
 console.log(sumamiento(1,2,3,4,5,6,7,8,9));
--- Practica2.02/Practica2.js
+++ Practica2.02/Practica2.js
@@ -1,8 +1,16 @@
 "use strict";
-function tablas(){
-    
-}
 
 function multiplicar(){
-
+    for (var i=10; i>0; i--){   //Bucle de líneas de tabla de multiplicar
+        console.log(`${arguments[0]} * ${i} = ${arguments[0]*[i]}`);    //Impresión de las tablas de multiplicar.
+    }
 }
+
+function tablas(){
+    for (var i=arguments[0]; i>1 ; i--){    //Bucle de ejecución de tablas de multiplicar
+        console.log(`Tabla del ${i}`);
+        multiplicar(i);
+    }
+}
+
+tablas(5,multiplicar);
