7c1bef0b1ca01c8570011bcd65a7e2ecd0635b28
AleEnjandro <alejandromartinez.alu@iespacomolla.es>
2021-12-20 15:17:13 +0100

    No estaban comitiados?

--- Practica7.04/personajes.js
+++ Practica7.04/personajes.js
@@ -5,29 +5,6 @@ var hayPag=true;
 var pag="https://swapi.dev/api/people";
 
 
-const promesa = new Promise((resolver, rechazar) => { // promesa con “petición al servidor”
-        fetch(pag, { // Dirección para realizar fetch
-        method:"GET",  // Establecemos método GET
-        headers: {  // Se indica en las cabeceras cómo es el contenido
-        'Content-type': 'application/x-www-form-urlencoded'
-        }
-        }).then((respuesta) =>{  // Código a ejecutar al recibir la respuesta
-            if (respuesta.ok) { // Si la respuesta es correcta
-                respuesta.text().then((datos) => { // Si se convierte a texto
-                    //todosPers = JSON.parse(datos); // Respuesta en un div
-                    todosPers.push(JSON.parse(datos));
-                    pag=todosPers[todosPers.length-1].next;
-                    console.log(pag);
-                    console.log(todosPers);
-                    if (pag==null){
-                        hayPag=false;
-                    }
-                });
-            }
-        });
-        resolver(todosPers);
-});
-
 var urlpers = "https://swapi.dev/api/people";
 // Petición de los datos para que sea menos caótico
 var peticion = new Request(urlpers, {
@@ -54,3 +31,9 @@ export const damePers = async () => {
     let personas = await personajes();
     console.log(personas);
 };
+
+export const damePersona = async (pagi) => {
+  urlpers=pagi;
+  let persona= await personajes();
+  console.log(persona);
+}
--- Practica7.04/planetas.js
+++ Practica7.04/planetas.js
@@ -1,1 +1,32 @@
 "use strict";
+
+export var todosPers=[];
+var hayPag=true;
+var pag="https://swapi.dev/api/planets";
+
+var urlpers = "https://swapi.dev/api/people";
+// Petición de los datos para que sea menos caótico
+var peticion = new Request(urlpers, {
+    method: "GET",
+    headers: new Headers({
+      "Content-type": "application/x-www-form-urlencoded",
+    }),
+});
+
+const planetas = () => {
+    return new Promise((resolver, rechazar) => {
+      fetch(peticion)
+        .then((respuesta) => respuesta.json())
+        .then((datos) => {
+          resolver(datos);
+        })
+        .catch(() => {
+          rechazar(new Error("Ha habido un error."));
+        });
+    });
+};
+
+export const damePlan = async () => {
+    let personas = await planetas();
+    console.log(personas);
+};
